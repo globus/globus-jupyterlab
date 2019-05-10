23fd2a089c08f57fc4e4ba546d1c800a25200220
Netta Gneezy <netta.en@gmail.com>
2019-05-10 13:03:17 -0700

    fixed all immediate issues (when running 'npm run build' command) except one, refactored file_manager to use normal timeout instead of invalid NodeJS.Timer

--- src/globus/api/search.ts
+++ src/globus/api/search.ts
@@ -1,50 +1,50 @@
-// import {GlobusSearchResult} from "./models";
-// import {makeGlobusRequest} from "./client";
-// import {Private} from "./client";
-// import tokens = Private.tokens;
+import {GlobusSearchResult} from "./models";
+import {makeGlobusRequest} from "./client";
+import {Private} from "./client";
+import tokens = Private.tokens;
 
-// const GLOBUS_SEARCH_API_URL = 'https://search.api.globus.org/';
+const GLOBUS_SEARCH_API_URL = 'https://search.api.globus.org/';
 
-// /**
-//  * Simple search
-//  * @param {string} index
-//  * @param {string} query
-//  * @returns {Promise<GlobusSearchResult>}
-//  */
-// export function searchIndex(index: string, query: string): Promise<GlobusSearchResult> {
-//     return makeSearchRequest(`${GLOBUS_SEARCH_API_URL}/v1/index/${index}/search?q=${query}`);
-// }
+/**
+ * Simple search
+ * @param {string} index
+ * @param {string} query
+ * @returns {Promise<GlobusSearchResult>}
+ */
+export function searchIndex(index: string, query: string): Promise<GlobusSearchResult> {
+    return makeSearchRequest(`${GLOBUS_SEARCH_API_URL}/v1/index/${index}/search?q=${query}`);
+}
 
-// /**
-//  * Advanced search with advanced queries. Recommended in order to do filtering.
-//  * @param {string} index
-//  * @param {string} query
-//  * @returns {Promise<GlobusSearchResult>}
-//  */
-// export function searchIndexAdvanced(index: string, query: string): Promise<GlobusSearchResult> {
-//     return makeSearchRequest(`${GLOBUS_SEARCH_API_URL}/v1/index/${index}/search?q=${query}&advanced=true`);
-// }
+/**
+ * Advanced search with advanced queries. Recommended in order to do filtering.
+ * @param {string} index
+ * @param {string} query
+ * @returns {Promise<GlobusSearchResult>}
+ */
+export function searchIndexAdvanced(index: string, query: string): Promise<GlobusSearchResult> {
+    return makeSearchRequest(`${GLOBUS_SEARCH_API_URL}/v1/index/${index}/search?q=${query}&advanced=true`);
+}
 
-// /**
-//  * Calls makeGlobusRequest() with the appropriate url and options to use the Globus Search API
-//  * @param {string} url
-//  * @param options
-//  * @returns {Promise<any>}
-//  */
-// function makeSearchRequest(url: string, options?: any): Promise<any> {
-//     options = {method: 'GET', ...options};
-//     options.headers = {'Authorization': `Bearer ${tokens.searchToken}`, ...options.headers};
+/**
+ * Calls makeGlobusRequest() with the appropriate url and options to use the Globus Search API
+ * @param {string} url
+ * @param options
+ * @returns {Promise<any>}
+ */
+function makeSearchRequest(url: string, options?: any): Promise<any> {
+    options = {method: 'GET', ...options};
+    options.headers = {'Authorization': `Bearer ${tokens.searchToken}`, ...options.headers};
 
-//     return makeGlobusRequest(url, options);
-// }
+    return makeGlobusRequest(url, options);
+}
 
-// /**
-//  * Takes in an object and encodes it in the form 'key1:value1 AND key2: value2'. Required for searchIndexAdvances()
-//  * @param params
-//  * @returns {string}
-//  */
-// export function searchQueryParams(params: any) {
-//     return Object.keys(params)
-//         .map(k => k + ':' + params[k])
-//         .join(' AND ');
-// }
+/**
+ * Takes in an object and encodes it in the form 'key1:value1 AND key2: value2'. Required for searchIndexAdvances()
+ * @param params
+ * @returns {string}
+ */
+export function searchQueryParams(params: any) {
+    return Object.keys(params)
+        .map(k => k + ':' + params[k])
+        .join(' AND ');
+}
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,35 @@
-// import {ILayoutRestorer, JupyterLab, JupyterLabPlugin} from '@jupyterlab/application';
-// import '../style/index.css';
-// import {GlobusHome} from "./globus/home";
-// import {IDocumentManager} from '@jupyterlab/docmanager';
-// import {IFileBrowserFactory} from "@jupyterlab/filebrowser";
-// import {GlobusWidgetManager} from "./globus/widget_manager";
+import {ILayoutRestorer, JupyterLab, JupyterLabPlugin} from '@jupyterlab/application';
+import '../style/index.css';
+import {GlobusHome} from "./globus/home";
+import {IDocumentManager} from '@jupyterlab/docmanager';
+import {IFileBrowserFactory} from "@jupyterlab/filebrowser";
+import {GlobusWidgetManager} from "./globus/widget_manager";
 
-// /**
-//  * Globus plugin
-//  */
-// export const globus: JupyterLabPlugin<void> = {
-//     id: '@jupyterlab/globus',
-//     autoStart: true,
-//     requires: [IDocumentManager, ILayoutRestorer, IFileBrowserFactory],
-//     activate: activateGlobus
-// };
+/**
+ * Globus plugin
+ */
+export const globus: JupyterLabPlugin<void> = {
+    id: '@jupyterlab/globus',
+    autoStart: true,
+    requires: [IDocumentManager, ILayoutRestorer, IFileBrowserFactory],
+    activate: activateGlobus
+};
 
-// function activateGlobus(app: JupyterLab, manager: IDocumentManager, restorer: ILayoutRestorer, factory: IFileBrowserFactory) {
-//     // Writes and executes a command inside of the default terminal in JupyterLab.
-//     // console.log(app.info);
-//     // app.serviceManager.terminals.startNew().then(session => {
-//     //     let request = `http POST ${window.location.href}/static body="$(printenv)" 'Authorization:token ${ServerConnection.defaultSettings.token}'\r`;
-//     //     session.send({ type: 'stdin', content: [request]});
-//     // });
+function activateGlobus(app: JupyterLab, manager: IDocumentManager, restorer: ILayoutRestorer, factory: IFileBrowserFactory) {
+    // Writes and executes a command inside of the default terminal in JupyterLab.
+    // console.log(app.info);
+    // app.serviceManager.terminals.startNew().then(session => {
+    //     let request = `http POST ${window.location.href}/static body="$(printenv)" 'Authorization:token ${ServerConnection.defaultSettings.token}'\r`;
+    //     session.send({ type: 'stdin', content: [request]});
+    // });
 
-//     let widgetManager: GlobusWidgetManager = new GlobusWidgetManager(app, manager, factory);
-//     let home: GlobusHome = new GlobusHome(widgetManager);
-//     restorer.add(home, 'globus-home');
-//     app.shell.addToLeftArea(home, { rank: 101 });
-// }
+    let widgetManager: GlobusWidgetManager = new GlobusWidgetManager(app, manager, factory);
+    let home: GlobusHome = new GlobusHome(widgetManager);
+    restorer.add(home, 'globus-home');
+    app.shell.addToLeftArea(home, { rank: 101 });
+}
 
-// /**
-//  * Export the plugin as default.
-//  */
-// export default globus;
+/**
+ * Export the plugin as default.
+ */
+export default globus;
