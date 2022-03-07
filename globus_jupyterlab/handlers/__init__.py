f28026016dc24f53564cfc8664eddef0835fa214
Nickolaus D. Saint <nick@globus.org>
2022-03-07 07:55:27 -0800

    Add type hints to init functions + docstrings.

--- globus_jupyterlab/handlers/__init__.py
+++ globus_jupyterlab/handlers/__init__.py
@@ -1,10 +1,13 @@
 import os
 import logging
+from types import ModuleType
+from typing import List, Tuple
 
 logging.basicConfig(level=logging.DEBUG)
 
 from notebook.utils import url_path_join
-
+from notebook.base.handlers import APIHandler
+from jupyter_server.serverapp import ServerWebApplication
 from tornado.web import StaticFileHandler
 from globus_jupyterlab.handlers import login, config
 
@@ -14,7 +17,16 @@ log = logging.getLogger(__name__)
 HANDLER_MODULES = (login, config)
 
 
-def get_handlers(modules, base_url, url_path) -> list:
+def get_handlers(modules: List[ModuleType], base_url: str, url_path: str) -> List[Tuple[str, APIHandler]]:
+    """
+    Fetch handlers from a list of modules. This style is taken from Jupyterhub,
+    which declares `default_handlers` on each of its handler modules and adds
+    them to a list here:
+    https://github.com/jupyterhub/jupyterhub/blob/main/jupyterhub/handlers/login.py#L168
+    Given a list of handler modules, this function will append the base_url and
+    url_path to each handler endpoint, which can then be passed straight to a
+    ServerWebApplication
+    """
     handlers = []
     for module in modules:
         for url, api_handler in module.default_handlers:
@@ -24,7 +36,11 @@ def get_handlers(modules, base_url, url_path) -> list:
     return handlers
 
 
-def setup_handlers(web_app, url_path):
+def setup_handlers(web_app: ServerWebApplication, url_path: str):
+    """
+    Setup main webapp handlers. Automatically adds all handlers
+    defined in HANDLER_MODULES.
+    """
     host_pattern = ".*$"
     base_url = web_app.settings["base_url"]
 
