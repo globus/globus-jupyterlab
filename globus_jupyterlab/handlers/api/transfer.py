a5494c0d9cfbadaa902c199b39e7813a141eb66b
Nickolaus D. Saint <nick@globus.org>
2022-03-23 14:06:21 -0700

    feat: Add operation_ls and endpoint_search server-extension endpoints

--- globus_jupyterlab/handlers/api/transfer.py
+++ globus_jupyterlab/handlers/api/transfer.py
@@ -7,6 +7,36 @@ from globus_jupyterlab.handlers.base import BaseAPIHandler
 from globus_jupyterlab.models import TransferModel
 
 
+class GetMethodTransferAPIEndpoint(BaseAPIHandler):
+
+    globus_sdk_method = None
+    mandatory_args = []
+    optional_args = {}
+
+    def get(self):
+        response = dict()
+        if self.login_manager.is_logged_in() is not True:
+            self.set_status(401)
+            return self.finish(json.dumps({'error': 'The user is not logged in'}))
+        try:
+            authorizer = self.login_manager.get_authorizer(
+                'transfer.api.globus.org')
+            tc = globus_sdk.TransferClient(authorizer=authorizer)
+            method = getattr(tc, self.globus_sdk_method)
+            args, kwargs = self.get_args()
+            response = method(*args, **kwargs)
+            return self.finish(json.dumps(response.data))
+        except globus_sdk.GlobusAPIError as gapie:
+            self.set_status(gapie.http_status)
+            return self.finish(json.dumps({'error': gapie.code, 'details': gapie.message}))
+
+    def get_args(self):
+        args = [self.get_query_argument(arg) for arg in self.mandatory_args]
+        kwargs = {arg: self.get_query_argument(arg, default)
+                  for arg, default in self.optional_args.items()}
+        return args, kwargs
+
+
 class SubmitTransfer(BaseAPIHandler):
     """An API Endpoint for submitting Globus Transfers."""
 
@@ -40,4 +70,32 @@ class SubmitTransfer(BaseAPIHandler):
             return self.finish(json.dumps({'error': 'Invalid Input', 'details': ve.json()}))
 
 
-default_handlers = [('/submit_transfer', SubmitTransfer, dict(), 'submit_transfer')]
+class OperationLS(GetMethodTransferAPIEndpoint):
+    """An API Endpoint doing a Globus LS"""
+    globus_sdk_method = 'operation_ls'
+    mandatory_args = ['endpoint']
+    optional_args = {
+        'path': None
+    }
+
+
+class EndpointSearch(GetMethodTransferAPIEndpoint):
+    """An API Endpoint for searching for collections"""
+    globus_sdk_method = 'endpoint_search'
+    mandatory_args = []
+    optional_args = {
+        'filter_fulltext': None,
+        'filter_scope': None,
+        'filter_owner_id': None,
+        'filter_host_endpoint': None,
+        'filter_non_functional': None,
+        'limit': None,
+        'offset': None,
+    }
+
+
+default_handlers = [
+    ('/submit_transfer', SubmitTransfer, dict(), 'submit_transfer'),
+    ('/operation_ls', OperationLS, dict(), 'operation_ls'),
+    ('/endpoint_search', EndpointSearch, dict(), 'endpoint_search'),
+]
