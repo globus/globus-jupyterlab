398409b621745f14c209620272539fc13a2f6e64
Nickolaus D. Saint <nick@globus.org>
2022-04-15 10:25:21 -0700

    refactor: Remove testing calls from main index.ts file

--- src/index.ts
+++ src/index.ts
@@ -73,54 +73,12 @@ export const globus: JupyterFrontEndPlugin<void> = {
 };
 
 async function activateGlobus(app: JupyterFrontEnd, factory: IFileBrowserFactory) {
-    // Writes and executes a command inside of the default terminal in JupyterLab.
-    // console.log(app.info);
-    // app.serviceManager.terminals.startNew().then(session => {
-    //     let request = `http POST ${window.location.href}/static body="$(printenv)" 'Authorization:token ${ServerConnection.defaultSettings.token}'\r`;
-    //     session.send({ type: 'stdin', content: [request]});
-    // });
     console.log('Globus Jupyterlab Extension Activated!');
 
-    console.log('JupyterLab extension server-extension-example is activated!');
-
-    // Ask for permission before starting transfers on startup.
-    const i_want_to_start_a_transfer_to_my_local_home_directory = false
     // GET request
     try {
       const data = await requestAPI<any>('config');
       console.log('Fetching basic data about the notebook server environment:', data);
-
-      const operation_ls = await requestAPI<any>('operation_ls?endpoint=ddb59aef-6d04-11e5-ba46-22000b92c6ec');
-      console.log('Info on collection', operation_ls);
-
-      const endpoint_search = await requestAPI<any>('endpoint_search?filter_fulltext=tutorial');
-      console.log('Endpoint Search: ', endpoint_search);
-
-      const endpoint_autoactivate = await requestAPI<any>('endpoint_autoactivate', {
-        body: JSON.stringify({'endpoint_id': 'ddb59aef-6d04-11e5-ba46-22000b92c6ec'}),
-        method: 'POST',
-      });
-      console.log(endpoint_autoactivate);
-
-      const transferRequest = {
-        //Globus Tutorial Endpoint 1
-        'source_endpoint': 'ddb59aef-6d04-11e5-ba46-22000b92c6ec',
-        'destination_endpoint': data.collection_id,
-        'transfer_items': [{
-          'source_path': '/share/godata',
-          'destination_path': '~/',
-          'recursive': true,
-        }]
-      };
-      
-      if (data.is_logged_in && i_want_to_start_a_transfer_to_my_local_home_directory) {
-        submitTransfer(transferRequest)
-      } else if (!data.is_logged_in) {
-        console.log('You are not logged in, visit http://localhost:8888/globus-jupyterlab/login to login')
-      } else if (!i_want_to_start_a_transfer_to_my_local_home_directory) {
-        // Consent is important
-        console.log('Aborting Transfer to your local home directory! Check index.ts for more info!')
-      }
 
       /*
       Commands to initiate a Globus Transfer. 
@@ -137,20 +95,6 @@ async function activateGlobus(app: JupyterFrontEnd, factory: IFileBrowserFactory
     } catch (reason) {
       console.error(`Error on GET /globus_jupyterlab/config.\n${reason}`);
     }
-}
-
-async function submitTransfer(transferRequest) {
-  try {
-    const reply = await requestAPI<any>('submit_transfer', {
-      body: JSON.stringify(transferRequest),
-      method: 'POST',
-    });
-    console.log(reply);
-  } catch (reason) {
-    console.error(
-      `Error on POST /jlab-ext-example/hello ${transferRequest}.\n${reason}`
-    );
-  }
 }
 
 /**
