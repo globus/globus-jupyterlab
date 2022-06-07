6f38f55ebb083be74460978c43e16218e9836aa6
Stephen Turoscy <sturoscy@globus.org>
2022-06-07 11:07:50 -0400

    change: formatting

--- src/components/Endpoint.tsx
+++ src/components/Endpoint.tsx
@@ -1,14 +1,13 @@
-import { Link, useLocation } from 'react-router-dom';
-import React, { useEffect, useState } from 'react';
-import { requestAPI } from '../handler';
-import { useHistory, useParams } from 'react-router-dom';
-import { useRecoilValue } from 'recoil';
+import { Link, useLocation } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { requestAPI } from "../handler";
+import { useHistory, useParams } from "react-router-dom";
+import { useRecoilValue } from "recoil";
 
-import { ConfigAtom } from './GlobusObjects';
-import { getBaseURL } from '../utilities';
-import { HubLogin } from './HubLoginWidget';
+import { ConfigAtom } from "./GlobusObjects";
+import { HubLogin } from "./HubLoginWidget";
 
-import * as path from 'path';
+import * as path from "path";
 var _path = path;
 
 const useQuery = () => {
@@ -43,13 +42,13 @@ const Endpoint = (props) => {
   // ComponentDidMount Functions
   useEffect(() => {
     const handleKeyUp = (event) => {
-      if (event.key === 'Shift') {
+      if (event.key === "Shift") {
         setShift(false);
       }
     };
-    document.addEventListener('keyup', handleKeyUp);
+    document.addEventListener("keyup", handleKeyUp);
     return () => {
-      document.removeEventListener('keyup', handleKeyUp);
+      document.removeEventListener("keyup", handleKeyUp);
     };
   }, []);
 
@@ -59,19 +58,11 @@ const Endpoint = (props) => {
         setShift(true);
       }
     };
-    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener("keydown", handleKeyDown);
     return () => {
-      document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener("keydown", handleKeyDown);
     };
   }, []);
-
-  useEffect(() => {
-    getEndpoint(endpointID);
-    return () => {
-      setAPIError(null);
-      setEndpoint(null);
-    };
-  }, [endpointID]);
 
   useEffect(() => {
     listEndpointItems(endpointID, path);
@@ -80,15 +71,6 @@ const Endpoint = (props) => {
       setEndpointList({ DATA: [], path: null });
     };
   }, [endpointID, path]);
-
-  const getEndpoint = async (endpointID) => {
-    try {
-      let endpoint = await requestAPI(`endpoint_detail?endpoint=${endpointID}`);
-      setEndpoint(endpoint);
-    } catch (error) {
-      setAPIError(error);
-    }
-  };
 
   // @ts-ignore
   const getSelectedFiles = () => {
@@ -109,36 +91,51 @@ const Endpoint = (props) => {
   };
 
   const listEndpointItems = async (endpointID, path = null) => {
+    setLoading(true);
     setAPIError(null);
     setEndpointList({ DATA: [], path: null });
-    setLoading(true);
+
+    let endpoint = await requestAPI(`endpoint_detail?endpoint=${endpointID}`);
+    if (!endpoint["activated"] && endpoint["expires_in"] == 0) {
+      let activated = await requestAPI<any>("endpoint_autoactivate", {
+        body: JSON.stringify({ endpoint_id: endpoint["id"] }),
+        method: "POST",
+      });
+      // Need to research if there is anything meaningful to do with the activation result
+      console.log(activated);
+    }
+    setEndpoint(endpoint);
+
     try {
-      var fullPath = query.get('full-path');
+      var fullPath = query.get("full-path");
       var url = `operation_ls?endpoint=${endpointID}`;
       if (fullPath) {
         url = `${url}&path=${fullPath}`;
       }
       const listItems = await requestAPI<any>(url);
+      setLoading(false);
       setEndpointList(listItems);
     } catch (error) {
+      setLoading(false);
       setAPIError({ ...error, ...{ global: true } });
+
       let error_response = await error.response.json();
 
       /*
         Note: This probably isn't a great UX to simply pop up a login page, but it
         does demonstrate the base functionality for picking endpoints
       */
-      if ('login_url' in error_response) {
+      if ("login_url" in error_response) {
         // Poll for successful authentication.
-        var lastConfig = await requestAPI<any>('config');
+        var lastConfig = await requestAPI<any>("config");
         var lastLogin = new Date(lastConfig.last_login).getTime();
 
         let authInterval = window.setInterval(async () => {
-          const updatedConfig = await requestAPI<any>('config');
+          const updatedConfig = await requestAPI<any>("config");
           var newLogin = new Date(updatedConfig.last_login).getTime();
 
           if (newLogin !== lastLogin) {
-            history.push('/');
+            history.push("/");
             history.replace(`/endpoints/${endpointID}`);
             clearInterval(authInterval);
           }
@@ -148,10 +145,17 @@ const Endpoint = (props) => {
           setAPIError(null);
           setLoginURL(error_response.login_url);
         } else {
-          window.open(getBaseURL(error_response.login_url), 'Globus Login', 'height=600,width=800').focus();
+          window
+            .open(
+              error_response.login_url,
+              "Globus Login",
+              "height=600,width=800"
+            )
+            .focus();
         }
       }
     }
+
     setLoading(false);
   };
 
@@ -164,7 +168,10 @@ const Endpoint = (props) => {
 
         let start = checkboxes.indexOf(lastChecked);
         let end = checkboxes.indexOf(event.target);
-        checked = checkboxes.slice(Math.min(start, end), Math.max(start, end) + 1);
+        checked = checkboxes.slice(
+          Math.min(start, end),
+          Math.max(start, end) + 1
+        );
 
         checked.forEach((el) => {
           el.checked = start < end ? true : false;
@@ -215,18 +222,25 @@ const Endpoint = (props) => {
     var sourceEndpoint = endpoint.id;
     var transferItems = [];
 
-    if (props.selectedJupyterItems.directories.length === 0 || props.selectedJupyterItems.directories.length > 1) {
+    if (
+      props.selectedJupyterItems.directories.length === 0 ||
+      props.selectedJupyterItems.directories.length > 1
+    ) {
       setLoading(false);
       setAPIError({
         response: {
-          status: 'DirectorySelectionError',
-          statusText: 'To transfer to Jupyter Hub, you must select only one directory to transfer to.',
+          status: "DirectorySelectionError",
+          statusText:
+            "To transfer to Jupyter Hub, you must select only one directory to transfer to.",
         },
       });
     } else {
       // Loop through selectedEndpointItems from state
       for (let selectedEndpointItem of selectedEndpointItems) {
-        let sourcePath = _path.posix.resolve(endpointList.path, selectedEndpointItem.name);
+        let sourcePath = _path.posix.resolve(
+          endpointList.path,
+          selectedEndpointItem.name
+        );
         let destinationPath = _path.posix.resolve(
           config.collection_base_path,
           props.selectedJupyterItems.directories[0].path,
@@ -236,7 +250,7 @@ const Endpoint = (props) => {
         transferItems.push({
           source_path: sourcePath,
           destination_path: destinationPath,
-          recursive: selectedEndpointItem.type == 'dir' ? true : false,
+          recursive: selectedEndpointItem.type == "dir" ? true : false,
         });
       }
 
@@ -247,9 +261,9 @@ const Endpoint = (props) => {
       };
 
       try {
-        const transferResponse = await requestAPI<any>('submit_transfer', {
+        const transferResponse = await requestAPI<any>("submit_transfer", {
           body: JSON.stringify(transferRequest),
-          method: 'POST',
+          method: "POST",
         });
         setLoading(false);
         setTransfer(transferResponse);
@@ -273,8 +287,9 @@ const Endpoint = (props) => {
     if (selectedEndpointItems.length > 1) {
       setAPIError({
         response: {
-          status: 'DirectorySelectionError',
-          statusText: 'Please only select one remote directory to transfer data to',
+          status: "DirectorySelectionError",
+          statusText:
+            "Please only select one remote directory to transfer data to",
         },
       });
     }
@@ -283,10 +298,17 @@ const Endpoint = (props) => {
     if (props.selectedJupyterItems.directories.length) {
       for (let directory of props.selectedJupyterItems.directories) {
         let destinationPath = selectedEndpointItems.length
-          ? _path.posix.resolve(endpointList.path, selectedEndpointItems[0].name, directory.path)
+          ? _path.posix.resolve(
+              endpointList.path,
+              selectedEndpointItems[0].name,
+              directory.path
+            )
           : _path.posix.resolve(endpointList.path, directory.path);
 
-        let sourcePath = _path.posix.resolve(config.collection_base_path, directory.path);
+        let sourcePath = _path.posix.resolve(
+          config.collection_base_path,
+          directory.path
+        );
 
         transferItems.push({
           source_path: sourcePath,
@@ -299,10 +321,17 @@ const Endpoint = (props) => {
     if (props.selectedJupyterItems.files.length) {
       for (let file of props.selectedJupyterItems.files) {
         let destinationPath = selectedEndpointItems.length
-          ? _path.posix.resolve(endpointList.path, selectedEndpointItems[0].name, file.path)
+          ? _path.posix.resolve(
+              endpointList.path,
+              selectedEndpointItems[0].name,
+              file.path
+            )
           : _path.posix.resolve(endpointList.path, file.path);
 
-        let sourcePath = _path.posix.resolve(config.collection_base_path, file.path);
+        let sourcePath = _path.posix.resolve(
+          config.collection_base_path,
+          file.path
+        );
 
         transferItems.push({
           source_path: sourcePath,
@@ -319,9 +348,9 @@ const Endpoint = (props) => {
     };
 
     try {
-      const transferResponse = await requestAPI<any>('submit_transfer', {
+      const transferResponse = await requestAPI<any>("submit_transfer", {
         body: JSON.stringify(transferRequest),
-        method: 'POST',
+        method: "POST",
       });
       setLoading(false);
       setTransfer(transferResponse);
@@ -333,18 +362,23 @@ const Endpoint = (props) => {
 
   if (apiError && apiError.global) {
     return (
-      <div className='alert alert-danger alert-dismissible col-8 fade show'>
+      <div className="alert alert-danger alert-dismissible col-8 fade show">
         <strong>
           Error {apiError.response.status}: {apiError.response.statusText}.
-        </strong>{' '}
+        </strong>{" "}
         {apiError.details && apiError.details}
-        <button type='button' className='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+        <button
+          type="button"
+          className="btn-close"
+          data-bs-dismiss="alert"
+          aria-label="Close"
+        ></button>
       </div>
     );
   }
 
   if (loading) {
-    return <h5 className='mt-3'>Loading</h5>;
+    return <h5 className="mt-3">Loading</h5>;
   }
 
   if (loginURL) {
@@ -353,77 +387,102 @@ const Endpoint = (props) => {
 
   return (
     <>
-      {endpointList['DATA'].length > 0 ? (
-        <div className='mt-3'>
-          <h5>Browsing Collection {endpoint ? endpoint.display_name : endpointID}</h5>
-          <div className='btn-group mb-4 mt-2'>
-            <button className='btn btn-sm btn-outline-primary' onClick={() => history.goBack()}>
-              <i className='fa-solid fa-turn-up'></i> Up one folder
+      {endpointList["DATA"].length > 0 ? (
+        <div className="mt-3">
+          <h5>
+            Browsing Collection {endpoint ? endpoint.display_name : endpointID}
+          </h5>
+          <div className="btn-group mb-4 mt-2">
+            <button
+              className="btn btn-sm btn-outline-primary"
+              onClick={() => history.goBack()}
+            >
+              <i className="fa-solid fa-turn-up"></i> Up one folder
             </button>
-            <button className='btn btn-sm btn-outline-primary' onClick={props.handleShowSearch}>
-              <i className='fa-solid fa-magnifying-glass'></i> Show search
+            <button
+              className="btn btn-sm btn-outline-primary"
+              onClick={props.handleShowSearch}
+            >
+              <i className="fa-solid fa-magnifying-glass"></i> Show search
             </button>
           </div>
 
           {transfer && (
-            <div className='alert alert-success alert-dismissible col-8 fade show'>
-              <h4 className='alert-heading'>Accepted!</h4>
-              <p>{transfer['message']}</p>
+            <div className="alert alert-success alert-dismissible col-8 fade show">
+              <h4 className="alert-heading">Accepted!</h4>
+              <p>{transfer["message"]}</p>
               <hr />
-              <p className='mb-0'>
+              <p className="mb-0">
                 <a
-                  className='alert-link'
-                  href={`https://app.globus.org/activity/${transfer['task_id']}`}
-                  target='_blank'>
-                  Check Status of Request <i className='fa-solid fa-arrow-up-right-from-square'></i>
+                  className="alert-link"
+                  href={`https://app.globus.org/activity/${transfer["task_id"]}`}
+                  target="_blank"
+                >
+                  Check Status of Request{" "}
+                  <i className="fa-solid fa-arrow-up-right-from-square"></i>
                 </a>
               </p>
-              <button type='button' className='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="alert"
+                aria-label="Close"
+              ></button>
             </div>
           )}
 
           {apiError && (
-            <div className='alert alert-danger alert-dismissible col-8 fade show'>
+            <div className="alert alert-danger alert-dismissible col-8 fade show">
               <strong>
-                Error {apiError.response.status}: {apiError.response.statusText}.
-              </strong>{' '}
+                Error {apiError.response.status}: {apiError.response.statusText}
+                .
+              </strong>{" "}
               {apiError.details && apiError.details}
-              <button type='button' className='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="alert"
+                aria-label="Close"
+              ></button>
             </div>
           )}
           <br />
 
-          <div id='endpoint-list' className='border col-8 rounded py-3'>
-            {endpointList['DATA'].map((listItem, index) => {
+          <div id="endpoint-list" className="border col-8 rounded py-3">
+            {endpointList["DATA"].map((listItem, index) => {
               return (
-                <div className='form-check ms-3' key={index}>
-                  {listItem['type'] == 'dir' ? (
+                <div className="form-check ms-3" key={index}>
+                  {listItem["type"] == "dir" ? (
                     <>
                       <input
-                        className='form-check-input'
-                        type='checkbox'
+                        className="form-check-input"
+                        type="checkbox"
                         value={JSON.stringify(listItem)}
                         ref={(el) => (itemsRef.current[index] = el)}
                         onChange={handleEndpointSelect}
-                        data-list-item-name={listItem['name']}></input>
+                        data-list-item-name={listItem["name"]}
+                      ></input>
                       <label>
                         <Link
-                          to={`/endpoints/${endpointID}/items/${listItem['name']}?full-path=${endpointList['path']}${listItem['name']}`}>
-                          <i className='fa-solid fa-folder-open'></i> {listItem['name']}
+                          to={`/endpoints/${endpointID}/items/${listItem["name"]}?full-path=${endpointList["path"]}${listItem["name"]}`}
+                        >
+                          <i className="fa-solid fa-folder-open"></i>{" "}
+                          {listItem["name"]}
                         </Link>
                       </label>
                     </>
                   ) : (
                     <>
                       <input
-                        className='form-check-input'
-                        type='checkbox'
+                        className="form-check-input"
+                        type="checkbox"
                         value={JSON.stringify(listItem)}
                         ref={(el) => (itemsRef.current[index] = el)}
                         onChange={handleEndpointSelect}
-                        data-list-item-name={listItem['name']}></input>
+                        data-list-item-name={listItem["name"]}
+                      ></input>
                       <label>
-                        <i className='fa-solid fa-file'></i> {listItem['name']}
+                        <i className="fa-solid fa-file"></i> {listItem["name"]}
                       </label>
                     </>
                   )}
@@ -432,20 +491,33 @@ const Endpoint = (props) => {
             })}
           </div>
 
-          <div id='transfer-direction'>
-            <div className='btn-group mt-4'>
-              <button type='button' className='btn btn-sm btn-outline-secondary' onClick={handleTransferToJupyter}>
-                <i className='fa-solid fa-arrow-left'></i> Transfer To JupyterLab
+          <div id="transfer-direction">
+            <div className="btn-group mt-4">
+              <button
+                type="button"
+                className="btn btn-sm btn-outline-secondary"
+                onClick={handleTransferToJupyter}
+              >
+                <i className="fa-solid fa-arrow-left"></i> Transfer To
+                JupyterLab
               </button>
-              <button type='button' className='btn btn-sm btn-outline-secondary' onClick={handleTransferFromJupyter}>
-                <i className='fa-solid fa-arrow-right'></i> Transfer From JupyterLab
+              <button
+                type="button"
+                className="btn btn-sm btn-outline-secondary"
+                onClick={handleTransferFromJupyter}
+              >
+                <i className="fa-solid fa-arrow-right"></i> Transfer From
+                JupyterLab
               </button>
             </div>
           </div>
         </div>
       ) : (
         <div>
-          <button className='btn btn-sm btn-primary mb-2 mt-3' onClick={() => history.goBack()}>
+          <button
+            className="btn btn-sm btn-primary mb-2 mt-3"
+            onClick={() => history.goBack()}
+          >
             Back
           </button>
           <p>No files or folders found</p>
--- src/components/HubLoginWidget.tsx
+++ src/components/HubLoginWidget.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { ReactWidget } from '@jupyterlab/apputils';
+import React, { useEffect, useRef, useState } from "react";
+import { ReactWidget } from "@jupyterlab/apputils";
 
-import { requestAPI } from '../handler';
+import { requestAPI } from "../handler";
 
 export const HubLogin = (props) => {
   const [apiError, setAPIError] = useState(null);
@@ -12,7 +12,7 @@ export const HubLogin = (props) => {
   useEffect(() => {
     // @ts-ignore
     hubLoginButton.current.disabled = true;
-  }, [])
+  }, []);
 
   const handleHubInputChange = (event) => {
     if (event.target.value) {
@@ -34,54 +34,91 @@ export const HubLogin = (props) => {
 
   if (apiError) {
     return (
-      <div className='alert alert-danger alert-dismissible col-8 fade show'>
+      <div className="alert alert-danger alert-dismissible col-8 fade show">
         <strong>
           Error {apiError.response.status}: {apiError.response.statusText}.
-        </strong>{' '}
+        </strong>{" "}
         {apiError.details && apiError.details}
-        <button type='button' className='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+        <button
+          type="button"
+          className="btn-close"
+          data-bs-dismiss="alert"
+          aria-label="Close"
+        ></button>
       </div>
     );
   }
 
   return (
-    <div className='container mt-3'>
-      <div className='row'>
-        <div className='col-8'>
+    <div className="container mt-3">
+      <div className="row">
+        <div className="col-8">
           {apiError && (
-            <div id='api-error' className='alert alert-danger'>
+            <div id="api-error" className="alert alert-danger">
               <strong>
-                Error {apiError.response.status}: {apiError.response.statusText}.
-              </strong>{' '}
+                Error {apiError.response.status}: {apiError.response.statusText}
+                .
+              </strong>{" "}
               Please try again.
             </div>
           )}
 
-          <label htmlFor='code-input' className='form-label'>
-            Paste Native App Authorization Code and Click Complete Login.
-            <br />
-            To start another Globus auth flow, click Login to Globus.
-          </label>
-          <input
-            type='text'
-            id='code-input'
-            className='form-control mb-3'
-            name='code-input'
-            onChange={handleHubInputChange}></input>
-          <div className='btn-group'>
-            <button type='button' className='btn btn-primary' ref={hubLoginButton} onClick={handleHubLogin}>
-              Complete Login
-            </button>
-            <button
-              type='button'
-              className='btn btn-outline-primary'
-              onClick={() => {
-                let loginURL = 'loginURL' in props ? props.loginURL : 'globus-jupyterlab/login';
-                window.open(loginURL, 'Login with Globus', 'height=600,width=800').focus();
-              }}>
-              Login to Globus
-            </button>
-          </div>
+          <ol className="list-group">
+            <li className="list-group-item d-flex justify-content-between align-items-start">
+              <div className="ms-2 me-auto">
+                <div className="fw-bold mb-3">
+                  1. Log In to Globus to obtain an Authorization Code for this
+                  transfer
+                </div>
+                <button
+                  type="button"
+                  className="btn btn-outline-primary"
+                  onClick={() => {
+                    let loginURL =
+                      "loginURL" in props
+                        ? props.loginURL
+                        : "globus-jupyterlab/login";
+                    window
+                      .open(
+                        loginURL,
+                        "Login with Globus",
+                        "height=600,width=800"
+                      )
+                      .focus();
+                  }}
+                >
+                  Log In to Globus
+                </button>
+              </div>
+            </li>
+
+            <li className="list-group-item">
+              <div className="ms-2 me-auto">
+                <div className="fw-bold mb-3">
+                  2. Copy and paste the Authorization Code you just received
+                  from Globus
+                </div>
+                <label htmlFor="code-input" className="form-label">
+                  Authorization Code
+                </label>
+                <input
+                  type="text"
+                  id="code-input"
+                  className="form-control mb-3"
+                  name="code-input"
+                  onChange={handleHubInputChange}
+                ></input>
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  ref={hubLoginButton}
+                  onClick={handleHubLogin}
+                >
+                  Continue
+                </button>
+              </div>
+            </li>
+          </ol>
         </div>
       </div>
     </div>
