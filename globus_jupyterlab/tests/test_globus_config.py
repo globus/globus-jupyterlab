18e6d7e9fc8ca43d2b474a309cdc8d553854866a
Nickolaus D. Saint <nick@globus.org>
2022-07-08 14:29:59 -0700

    chore: Add tests for globus config

--- globus_jupyterlab/tests/test_globus_config.py
+++ globus_jupyterlab/tests/test_globus_config.py
@@ -65,3 +65,61 @@ def test_check_env_boolean(case, default, env_value, expected, monkeypatch):
             GlobusConfig().check_env_boolean("ENV_VALUE", default)
     else:
         assert GlobusConfig().check_env_boolean("ENV_VALUE", default) == expected, case
+
+
+def test_get_refresh_tokens(monkeypatch):
+    monkeypatch.setenv("GLOBUS_REFRESH_TOKENS", "true")
+    assert GlobusConfig().get_refresh_tokens() is True
+
+
+def test_transfer_is_hub_service(monkeypatch):
+    monkeypatch.setenv("GLOBUS_TRANSFER_SUBMISSION_IS_HUB_SERVICE", "true")
+    assert GlobusConfig().get_transfer_submission_is_hub_service() is True
+
+
+def test_get_last_login():
+    gc = GlobusConfig()
+    gc.last_login = "yesterday"
+    assert gc.last_login == "yesterday"
+
+
+def test_get_scopes():
+    gc = GlobusConfig()
+    assert gc.get_scopes() == [
+        "urn:globus:auth:scope:transfer.api.globus.org:all",
+        "profile",
+        "openid",
+    ]
+
+
+def test_get_scopes_with_transfer(monkeypatch):
+    monkeypatch.setenv("GLOBUS_TRANSFER_SUBMISSION_URL", "myservice")
+    monkeypatch.setenv("GLOBUS_TRANSFER_SUBMISSION_SCOPE", "myscope")
+    gc = GlobusConfig()
+    assert gc.get_scopes() == [
+        "urn:globus:auth:scope:transfer.api.globus.org:all",
+        "profile",
+        "openid",
+        "myscope",
+    ]
+
+
+def test_get_named_grant_default():
+    assert GlobusConfig().get_named_grant() == "Globus JupyterLab"
+
+
+def test_set_custom_scope_without_url(monkeypatch):
+    monkeypatch.setenv("GLOBUS_TRANSFER_SUBMISSION_SCOPE", "myscope")
+    with pytest.raises(ValueError):
+        GlobusConfig().get_transfer_submission_scope()
+
+
+def test_get_redirect_uri(mock_hub_env):
+    assert (
+        GlobusConfig().get_redirect_uri() == "https://auth.globus.org/v2/web/auth-code"
+    )
+
+
+def test_gcp_owner_is_none(gcp):
+    gcp.get_owner_info.return_value = None
+    assert GlobusConfig().get_collection_id_owner() is None
