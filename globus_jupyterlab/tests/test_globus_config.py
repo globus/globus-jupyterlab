a1123b4dc598c607f8def7dc09808c93ef410e46
Nickolaus D. Saint <nick@globus.org>
2022-07-08 13:55:25 -0700

    Add tests for env checks

--- globus_jupyterlab/tests/test_globus_config.py
+++ globus_jupyterlab/tests/test_globus_config.py
@@ -1,3 +1,4 @@
+import pytest
 from globus_jupyterlab.globus_config import GlobusConfig
 
 
@@ -17,3 +18,50 @@ def test_non_gcp_collection(monkeypatch, gcp):
     monkeypatch.setenv("GLOBUS_COLLECTION_ID", "i_am_a_token!")
     gcp.endpoint_id = "my_endpoint"
     assert GlobusConfig().is_gcp() is False
+
+
+@pytest.mark.parametrize(
+    "case, default, env_value, expected",
+    [
+        (
+            "Env correctly set to True, defalut is true, True expected",
+            True,
+            "true",
+            True,
+        ),
+        (
+            "Env set to True, defalut is False, false expected",
+            True,
+            "false",
+            False,
+        ),
+        ("No value provided, default is True, True Expected", True, "", True),
+        (
+            "No value provided, default is True. False Expected",
+            False,
+            "",
+            False,
+        ),
+        (
+            "Env set to fruitloops, default is False. ValueError Exception Expected",
+            False,
+            "fruitloops",
+            ValueError,
+        ),
+        (
+            "Env set to coacoa puffs, default is true. ValueError Exception Expected",
+            True,
+            "coacoa puffs",
+            ValueError,
+        ),
+    ],
+)
+def test_check_env_boolean(case, default, env_value, expected, monkeypatch):
+
+    if env_value != "":
+        monkeypatch.setenv("ENV_VALUE", env_value)
+    if expected == ValueError:
+        with pytest.raises(ValueError):
+            GlobusConfig().check_env_boolean("ENV_VALUE", default)
+    else:
+        assert GlobusConfig().check_env_boolean("ENV_VALUE", default) == expected, case
